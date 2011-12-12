6b08ab3d5585fe4d2c00fe50a6a7f57ad9999122
Jonathan Walker <jonnie.l.walker@gmail.com>
2011-12-11 18:51:05 -0600

    Initial commit from redo of finalproject

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Final::Application.config.secret_token = '66534105312d958249063ba6b41e8b453be47b5528e6a707e3cd9bb61cdfbab2f6c3cf75827365e0c547ace2586e93735dbffc796c3503acd90ca1925d1d651a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Final::Application.config.session_store :cookie_store, key: '_final_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Final::Application.config.session_store :active_record_store
